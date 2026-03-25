290a37e1027719b5dedccc980bf138f9f939fe79
Athira S <athiras8593@gmail.com>
2026-03-25 18:55:02 +0530

    changes in report

--- reports/urls.py
+++ reports/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.publications, name='publications'),
+    path('legalmanualenglish/', views.legal_manual_english,
+         name='legal_manual_english'),
+    path('legalmanualmalayalm/', views.legal_manual_malayalm,
+         name='legal_manual_malayalm')
 ]
--- reports/views.py
+++ reports/views.py
@@ -7,3 +7,11 @@ def publications(request):
     return render(request, 'reports/publications.html', {
         'latest_year': latest_year,
     })
+
+
+def legal_manual_english(request):
+    return render(request, 'reports/legal_manual_english.html')
+
+
+def legal_manual_malayalm(request):
+    return render(request, 'reports/legal_manual_malayalm.html')
